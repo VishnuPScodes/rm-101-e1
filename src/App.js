0236a4c4e00969155f26a9032f2038cf2a1e18c3
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-07-15 14:14:20 +0530

    add task added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { AddTask } from "./components/AddTask";
 function App() {
-  return <div>{/* Code Here */}</div>;
+  return <div>{/* Code Here */}
+  <AddTask/>
+  </div>;
 }
 
 export default App;
--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import styles from "./addTask.module.css";
 
 const AddTask = () => {
+  const [data,setData]=useState('');
   // NOTE: do not delete `data-testid` key value pair
+  const handleChange=((e)=>{
+     
+    setData(e.target.value)
+  })
+  const handleSubmit=(()=>{
+    var datas={
+        text:data,
+        done:false,
+        counter:1
+
+    }
+    if(data==''){
+      return alert('please give something')
+    }
+    axios.post('http://localhost:8080/todos',datas).then((e)=>{
+      alert('Added')
+    })
+    console.log(data)
+  })
   return (
     <div className={styles.todoForm}>
-      <input data-testid="add-task-input" type="text" />
-      <button data-testid="add-task-button"></button>
+      <input data-testid="add-task-input"  type="text" onChange={handleChange} />
+      <button data-testid="add-task-button" onClick={handleSubmit}>Add task</button>
     </div>
   );
 };
