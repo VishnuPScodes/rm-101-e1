bc265d0f052c364ba2fa5a1d8f513ab9a9dc4540
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-07-15 16:00:28 +0530

    Update Tasks.jsx

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./tasks.module.css";
-
+import axios from 'axios'
 const Tasks = () => {
+  const [data,setData]=useState([])
   // NOTE: do not delete `data-testid` key value pair
+  useEffect(()=>{
+     axios.get('http://localhost:8081/todos').then((e)=>{
+        setData(e.data)
+     })
+  },[])
+  console.log(data)
   return (
     <>
       <ul data-testid="tasks" className={styles.tasks}>
